a1f13fef2817266dec723a9295874430a6c46296
spettigrew <sara.pettigrew5@gmail.com>
2020-02-06 15:48:25 -0700

    Setting up route testing functions, not passing

--- foodRequest/foodRouter.spec.js
+++ foodRequest/foodRouter.spec.js
@@ -3,13 +3,38 @@ const server = require("../server")
 const db = require("../database/dbConfig")
 
 beforeAll(async () => {
-    await db.seed.truncate()
+    await db.seed.run()
 })
 
-test("welcome route", async () => {
-    const res = await supertest(server).get("/")
+describe("food request router tests", () => {
+    test("welcome route", async () => {
+        const res = await supertest(server).get("/")
 
-    expect(res.status).toBe(201)
-    expect(res.type).toBe("application/json")
-    expect(res.body.message).toBe("Welcome to Replate!")
+        expect(res.status).toBe(201)
+        expect(res.type).toBe("application/json")
+        expect(res.body.message).toBe("Welcome to Replate!")
+    })
+
+    test("create food request route", async () => {
+        const res = await supertest(server)
+            .post("/api/foodRequest")
+            .send({
+                type: "Sandwich"
+            })
+        expect(res.status).toBe(201)
+        expect(res.type).toBe("application/json")
+        expect(res.body.username).toBe(type, "Sandwich")
+    })
+
+    test("check id and name of food request", async () => {
+        const res = await supertest(server)
+            .get("api/foodRequest/:id")
+            .send({
+                id: 2,
+                name: "Salad",
+            })
+        expect(res.status).toBe(200)
+        expect(res.type).toBe("application/json")
+        expect(res.body).toBe(id, "2", name, "Salad")
+    })
 })
--- volunteers/volunteerRouter.spec.js
+++ volunteers/volunteerRouter.spec.js
@@ -3,7 +3,7 @@ const server = require("../server")
 const db = require("../database/dbConfig")
 
 beforeAll(async () => {
-    await db.seed.truncate()
+    await db.seed.run()
 })
 
 // let token = '';
@@ -21,31 +21,44 @@ beforeAll(async () => {
 //         })
 // })
 
-test("welcome route", async () => {
-    const res = await supertest(server).get("/")
+describe("volunteer router tests", () => {
+    test("welcome route", async () => {
+        const res = await supertest(server).get("/")
+        expect(res.status).toBe(201)
+        expect(res.type).toBe("application/json")
+        expect(res.body.message).toBe("Welcome to Replate!")
+    })
 
-    expect(res.status).toBe(201)
-    expect(res.type).toBe("application/json")
-    expect(res.body.message).toBe("Welcome to Replate!")
-})
+    test("create volunteer user route", async () => {
+        const res = await supertest(server)
+            .post("/api/volunteers/register")
+            .send({ 
+                username: "sam", 
+                password: "$2a$10$zY9/yBf0MYWGGtiEZrFQ8ef1KYLFPAmguEk3tX2NWP1mBhdekcj8" })
+        expect(res.status).toBe(201)
+        expect(res.type).toBe("application/json")
+        expect(res.body.username).toBe("sam")
+    })
 
-test("create volunteer user route", async () => {
-    const res = await supertest(server)
-        .post("/api/volunteers/register")
-        .send({ username: "sam", password: "abc123" })
+    test("check volunteer login status", async () => {
+        const res = await supertest(server)
+            .post("/api/volunteers/login")
+            .send({ name: "Sara" })
 
-    expect(res.status).toBe(200)
-    expect(res.type).toBe("application/json")
-    expect(res.body.username).toBe("sam")
-})
+        expect(res.status).toBe(200)
+        expect(res.type).toBe("application/json")
+        expect(res.body).toBe(name, "Sara")
+    })
 
-test("check volunteer login status", async () => {
-    const res = await supertest(server)
-        .post("/api/auth/login")
-        .send({ username: "joseph" })
-
-    expect(res.status).toBe(200)
-    expect(res.type).toBe("application/json")
-    expect(res.body).toBe(username, "joseph")
-
+    test("check id and name of volunteer", async () => {
+        const res = await supertest(server)
+            .get("api/volunteers/login/:id")
+            .send({
+                id: 5,
+                name: "Sara",
+            })
+        expect(res.status).toBe(200)
+        expect(res.type).toBe("application/json")
+        expect(res.body).toBe(id, "5", name, "Sara")
+    })
 })
