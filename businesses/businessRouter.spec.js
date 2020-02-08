eb46f4172b536bf358ccf9b6fc0f28c9f7521780
spettigrew <sara.pettigrew5@gmail.com>
2020-02-07 17:41:46 -0700

    Fixing testing routes; nearly half are passing

--- businesses/businessRouter.spec.js
+++ businesses/businessRouter.spec.js
@@ -18,35 +18,46 @@ describe("business router tests", () => {
         const res = await supertest(server)
             .post("/api/businesses/register")
             .send({ 
-                username: "nick", 
-                password: "$2a$10$zY9/yBf0MYWGGtiEZrFQ8ef1KYLFPAmguEk3tX2NWP1mBhdekcj8" })
+                username: "skyelar20", 
+                password: "abc123" })
+                //console.log(res.body)
+        const token = res.body.token
         expect(res.status).toBe(201)
         expect(res.type).toBe("application/json")
-        expect(res.body.username).toEqual("nick")
+        expect(res.body).toEqual({ token, message: "Welcome skyelar20!" })
     })
 
     test("check business login status", async () => {
         const res = await supertest(server)
             .post("/api/businesses/login")
             .send({ 
-                username: "nick", 
-                password: "$2a$10$zY9/yBf0MYWGGtiEZrFQ8ef1KYLFPAmguEk3tX2NWP1mBhdekcj8" })
+                username: "skyelar20", 
+                password: "abc123" })
         //console.log(res)
+        const token = res.body.token
         expect(res.status).toBe(200)
         expect(res.type).toBe("application/json")
-        expect(res.body).toBe(username, "nick", password, "$2a$10$zY9/yBf0MYWGGtiEZrFQ8ef1KYLFPAmguEk3tX2NWP1mBhdekcj8")
+        expect(res.body).toEqual({ message: "Welcome skyelar20!", token })
 
     })
 
-    test("check id and name of business user", async () => {         
+    test("check id and name of business user", async () => {   
+        const login = await supertest(server)
+            .post("/api/businesses/login")
+            .send({ 
+                username: "sara",
+                password: "abc123"
+            })
         const res = await supertest(server)
-        .get("api/businesses/:id")
+        .get("api/businesses/2")
             .send({
-                id: 2,
                 name: "Panda Express",
                  })
+            .set (
+                "token", login.body.token
+            )
             expect(res.status).toBe(200)
             expect(res.type).toBe(application/json)
-            expect(res.body).toBe(id, "2", name, "Panda Express" )
+            expect(res.body).toBe({ name: "Panda Express" })
     })
 })
