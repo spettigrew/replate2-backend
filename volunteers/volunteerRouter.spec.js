9a390578a1718d30573ea17ad4f1916cbf7b8968
spettigrew <sara.pettigrew5@gmail.com>
2020-02-07 19:40:17 -0700

    Modifying test function

--- volunteers/volunteerRouter.spec.js
+++ volunteers/volunteerRouter.spec.js
@@ -31,15 +31,21 @@ describe("volunteer router tests", () => {
             .post("/api/volunteers/login")
             .send({ 
                 name: "skyelar5",
-                password: "abc123"
+                password: "abc123",
             })
         const token = res.body.token
         expect(res.status).toBe(200)
         expect(res.type).toBe("application/json")
-        expect(res.body).toEqual({ token, message: "Welcome skyelar5!" })
+        expect(res.body).toEqual({ message: "Welcome skyelar5!", token })
     })
 
     test("check id and name of volunteer", async () => {
+        const login = await supertest(server)
+            .post("/api/volunteers/login")
+            .send({
+                username: "skyelar5",
+                password: "abc123"
+            })
         const res = await supertest(server)
             .get("api/volunteers/5")
             .send({
